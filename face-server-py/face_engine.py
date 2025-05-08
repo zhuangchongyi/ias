6584db5e7ea51ebeaebece207b7a258f23ff23c9
zhuangchongyi <1909017815@qq.com>
2025-05-08 15:04:01 +0800

    feat: 添加人脸检测功能，支持返回人脸坐标和标记人脸的图像

--- face-server-py/face_engine.py
+++ face-server-py/face_engine.py
@@ -1,3 +1,5 @@
+from io import BytesIO
+
 import cv2
 import numpy as np
 from fastapi import UploadFile
@@ -6,7 +8,7 @@ from insightface.app import FaceAnalysis
 
 class FaceEngine:
     def __init__(self):
-        self.model = FaceAnalysis(name="buffalo_l", providers=["CPUExecutionProvider"])
+        self.model = FaceAnalysis(name="buffalo_l", providers=["CUDAExecutionProvider", "CPUExecutionProvider"])
         self.model.prepare(ctx_id=0)
 
     def extract_embedding(self, file: UploadFile) -> np.ndarray:
@@ -19,3 +21,40 @@ class FaceEngine:
         if not faces:
             raise Exception("没有检测到人脸")
         return faces[0].embedding
+
+    def detect_face(self, content):
+        content_buf = np.frombuffer(content, np.uint8)
+        img = cv2.imdecode(content_buf, cv2.IMREAD_COLOR)
+
+        faces = self.model.get(img)
+        results = []
+        for face in faces:
+            box = face.bbox.astype(int).tolist()
+            results.append({
+                'x': box[0],
+                'y': box[1],
+                'width': box[2] - box[0],
+                'height': box[3] - box[1]
+            })
+
+        return results
+
+    def detect_face_image(self, content):
+        np_img = np.frombuffer(content, np.uint8)
+        img = cv2.imdecode(np_img, cv2.IMREAD_COLOR)
+
+        detections = self.detect_face(content)
+        if len(detections) == 0:
+            raise Exception("没有检测到人脸")
+
+        for det in detections:
+            x1 = det["x"]
+            y1 = det["y"]
+            x2 = x1 + det["width"]
+            y2 = y1 + det["height"]
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+        # 编码为 JPEG 并写入 BytesIO
+        _, buffer = cv2.imencode('.jpg', img)
+        byte_io = BytesIO(buffer.tobytes())
+        return byte_io
--- face-server-py/main.py
+++ face-server-py/main.py
@@ -3,6 +3,7 @@ from typing import Union
 
 from fastapi import FastAPI, File, UploadFile
 from fastapi import Query
+from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
 
 from face_engine import FaceEngine
@@ -28,6 +29,12 @@ def error(msg="操作失败", data=None):
 
 @app.post("/register_face")
 def register_face(file: UploadFile = File(...), user_id: str = Query(...)):
+    """
+    注册人脸
+    :param file: 人脸图像
+    :param user_id: 用户ID
+    :return: 成功识别人脸信息返回人脸id
+    """
     try:
         embedding = face_engine.extract_embedding(file)
         if embedding is None:
@@ -43,6 +50,11 @@ def register_face(file: UploadFile = File(...), user_id: str = Query(...)):
 
 @app.post("/compare_face")
 def compare_face(file: UploadFile = File(...)):
+    """
+    人脸比对
+    :param file:  人脸图像
+    :return: 符合人脸匹配的用户信息
+    """
     try:
         embedding = face_engine.extract_embedding(file)
         search_params = {"metric_type": "COSINE", "params": {"nprobe": 10}}
@@ -68,6 +80,11 @@ def compare_face(file: UploadFile = File(...)):
 
 @app.post("/delete_face")
 def delete_face(body: FaceData):
+    """
+    删除人脸
+    :param body: 依据用户ID和人脸ID列表的请求体
+    :return: 是否删除成功
+    """
     try:
         # 构建表达式
         id_list_str = ', '.join([f'\"{u}\"' for u in body.id_list])
@@ -75,5 +92,35 @@ def delete_face(body: FaceData):
         # 删除数据
         mutation_result = face_collection.delete(expr)
         return success(data=mutation_result.delete_count > 0)
+    except Exception as e:
+        return error(msg=str(e), data=False)
+
+
+@app.post("/detect_face")
+async def detect_face(file: UploadFile = File(...)):
+    """
+    检测人脸
+    :param file: 人脸图像
+    :return: 人脸坐标
+    """
+    try:
+        content = await file.read()
+        coordinate = face_engine.detect_face(content)
+        return success(data=coordinate)
+    except Exception as e:
+        return error(msg=str(e), data=False)
+
+
+@app.post("/detect_face_image")
+async def detect_face_image(file: UploadFile = File(...)):
+    """
+    检测人脸并返回标记人脸的图像
+    :param file: 人脸图像
+    :return: 标记人脸的图像
+    """
+    try:
+        content = await file.read()
+        result_img = face_engine.detect_face_image(content)
+        return StreamingResponse(result_img, media_type="image/jpeg")
     except Exception as e:
         return error(msg=str(e), data=False)
